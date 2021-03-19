f8ffb2a92a0b6cdd9ff2d82d1381dda23ce1e7db
Austin Gray <argray17@gmail.com>
2021-03-18 23:36:26 -0500

    Finished tutorial

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const exphbs = require('express-handlebars')
 const PORT = process.env.PORT || 5000
 const members = require('./Members')
+
 const logger = require('./middleware/logger')
 
 //Init middleware
 app.use(logger)
 
-//Route to get all members
-app.get('/api/members', (req, res) => res.json(members))
+//Init handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
-//Route to get single member
-//app.get()
+//Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//Home page render
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members,
+  })
+)
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+//Members api routes
+app.use('/api/members', require('./routes/api/members'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
